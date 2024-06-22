1aaf9227c16e952c62c4591049b899e215c7a993
bewithsnehasish <mandalsneh97@gmail.com>
2024-06-22 19:40:37 +0530

    Initializing the database

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const path = require("path");
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+
 
 main()
   .then(() => {
@@ -33,6 +37,14 @@ app.get("/testListing", async (req, res) => {
   console.log("Sample was saved Sucessfully");
   res.send("Successful testing");
 });
+
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({})
+  res.render("listings/index.ejs",{allListings});
+});
+
+
+app.get("/listings/")
 
 app.listen(8080, () => {
   console.log("Server is listening to port 8080");
