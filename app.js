a38ec578dbb38b660ff11639d5a89d80ade5d014
bewithsnehasish <mandalsneh97@gmail.com>
2024-06-29 20:55:49 +0530

    Update the backend

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
-
+app.use(express.urlencoded({extended:true}))
 
 main()
   .then(() => {
@@ -38,13 +38,24 @@ app.get("/testListing", async (req, res) => {
   res.send("Successful testing");
 });
 
+//Index Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({})
   res.render("listings/index.ejs",{allListings});
 });
 
+//Show route
+app.get("/listings/:id",async (req, res)=> {
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs",{listing})
+})
 
-app.get("/listings/")
+//New route
+app.get("/listings/new",(req,res)=>{
+  
+})
+
 
 app.listen(8080, () => {
   console.log("Server is listening to port 8080");
